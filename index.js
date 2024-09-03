5756903609ce4a1000d2fa4ac9a43cf4470e923c
wiktor-nowak <nowak.wiktor95@gmail.com>
2024-09-03 23:55:43 +0200

    basics of RXJS added

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+const users = {
+  data: [
+    {
+      status: "active",
+      age: 14
+    },
+    {
+      status: "active",
+      age: 32
+    },
+    {
+      status: "active",
+      age: 4
+    },
+    {
+      status: "inactive",
+      age: 11
+    },
+    {
+      status: "inactive",
+      age: 23
+    },
+    {
+      status: "active",
+      age: 13
+    },
+    {
+      status: "inactive",
+      age: 28
+    }
+  ]
+};
 
 const observable = new Observable((subscriber) => {
-  subscriber.next(10);
-});
+  subscriber.next(users);
+}).pipe(
+  map((value) => value.data),
+  map((value) => value.filter((user) => user.status === "active")),
+  map((value) => value.reduce((sum, user) => sum + user.age, 0) / value.length),
+  map((value) => {
+    if (value < 18) {
+      throw new Error("Average is less than 18!");
+    } else return value;
+  })
+);
 
 const observer = {
   // it implements the 3 defined below methods
